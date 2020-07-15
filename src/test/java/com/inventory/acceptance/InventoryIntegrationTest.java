48856314ab9c5cd57455bb06a5cc29949153a188
oaparnav <oaparnav@ford.com,ptamils3@ford.com>
2020-07-15 15:26:14 +0530

    Fixed failing testcases

--- src/test/java/com/inventory/acceptance/InventoryIntegrationTest.java
+++ src/test/java/com/inventory/acceptance/InventoryIntegrationTest.java
@@ -1,17 +1,36 @@
 package com.inventory.acceptance;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.inventory.model.ProductRequest;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,6 +39,28 @@ public class InventoryIntegrationTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+	
+	private ProductRequest user;
+	
+	private static ObjectMapper mapper;
+	
+	private MockHttpServletRequestBuilder requestBuilder;
+	
+	private MockRestServiceServer mockRestServiceServer;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Before
+	public void setup() throws FileNotFoundException, IOException {
+		Gson gson = new Gson();
+		ClassPathResource classPathResource = new ClassPathResource("addProduct.json");
+		JsonReader reader = new JsonReader(new FileReader(classPathResource.getFile()));
+		user = gson.fromJson(reader, ProductRequest.class);
+		mapper = new ObjectMapper();
+		
+		mockRestServiceServer =  MockRestServiceServer.createServer(restTemplate);
+	}
 
 	@Test
 	public void shouldReturnProductInformationForItemValidProductId() throws Exception {
@@ -47,6 +88,18 @@ public class InventoryIntegrationTest {
 				.andExpect(jsonPath("$[1].item.price").value(150))
 				.andExpect(jsonPath("$[1].item.quantity").value(5))
 				.andExpect(jsonPath("$[1].item.shippingPrice").value(50));
+	}
+	
+	@Test
+	public void shouldReturnSuccessForAddPrdocutDetails() throws Exception {
+		
+		mockRestServiceServer.expect(MockRestRequestMatchers.requestTo(containsString("/catalog/v1/product/")))
+		.andRespond(withSuccess());
+		requestBuilder = post("/v1/item/");
+		requestBuilder.content(mapper.writeValueAsString(user));
+		requestBuilder.contentType(MediaType.APPLICATION_JSON);
+		mockMvc.perform(requestBuilder).andDo(print()).andExpect(status().isOk());
+
 	}
 
 }
